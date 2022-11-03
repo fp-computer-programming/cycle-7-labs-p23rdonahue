1216c63a7868da34769bba703e4dc20848f4df56
Robbie Donahue <p23rdonahue@fairfieldprep.org>
2022-11-03 10:56:07 -0400

    lab 6-1

---
+++ lab_6-1.py
@@ -0,0 +1,12 @@
+#author: RED 11/3/22
+#making list
+my_lst = [1,2,3,4,5]
+#finding 2 or rather third piece in list
+print(my_lst[2])
+#finding length
+print(len(my_lst))
+#repeating list
+print(my_lst * 3)
+#making string an array
+str= "string"
+print(list(str))
